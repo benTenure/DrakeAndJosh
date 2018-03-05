dcbfaff445b96501a97c1f3129999a88ad7972c4
Unknown <ben.mozilla@gmail.com>
2018-03-04 23:46:23 -0500

    -"Letter Block" spawns with randomized letter -Changed collider on Drake to pill instead -Added assets(?)

---
+++ Baby Dragon Assasin/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+	//[SerializeField] private Texture2D[] textures;
+	[SerializeField] private Sprite[] sheet = new Sprite[26];
+
+	private int minTexture = 0;
+	private int maxTexture = 25;
+
+	SpriteRenderer m_SpriteRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+		m_SpriteRenderer = GetComponent<SpriteRenderer>();
+		int randomInt = Random.Range(minTexture, maxTexture);
+		m_SpriteRenderer.sprite = sheet[randomInt];
+
+
+	}
+}
