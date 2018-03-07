9bc5aada6a3d93c008ae59443e21fb815a453308
Unknown <ben.mozilla@gmail.com>
2018-03-06 23:59:14 -0500

    Testing scripts

---
+++ Baby Dragon Assasin/Assets/Scripts/ShakeyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeyScript : MonoBehaviour {
+
+	bool Up;
+	float timer;
+
+	void OnCollisionEnter2d(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			if (timer <= 2){
+				if(Up) {
+					transform.Translate(0,0.1f,0);
+					Up = false;
+				} else {
+					transform.Translate(0, 0.1f, 0);
+					Up = true;
+				}
+			}
+			timer += Time.deltaTime;
+			Debug.Log(timer);
+		}
+	}
+}
