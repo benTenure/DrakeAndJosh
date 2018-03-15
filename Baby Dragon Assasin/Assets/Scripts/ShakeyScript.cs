24aa7e6786e4b01f228378fe9d9779e31590c651
Unknown <ben.mozilla@gmail.com>
2018-03-15 01:32:16 -0400

    Fixing Animations
    Got the platfors to fall one second after player touches them, but the animations still don't get called (not even one time!?)

--- Baby Dragon Assasin/Assets/Scripts/ShakeyScript.cs
+++ Baby Dragon Assasin/Assets/Scripts/ShakeyScript.cs
@@ -8,16 +8,28 @@ public class ShakeyScript : MonoBehaviour {
     Rigidbody2D rb;
     int shakeHash = Animator.StringToHash("Platform_Shake");
 
+    public float fallDelay = 2.0f;
+
 	private void Start()
 	{
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
 	}
 
-	private void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.tag == "Player") {
+	private void OnCollisionEnter2D(Collision2D coll) 
+    {
+		if (coll.gameObject.tag == "Player") 
+        {
             anim.SetTrigger(shakeHash);
-            rb.bodyType = RigidbodyType2D.Dynamic;
+            //rb.bodyType = RigidbodyType2D.Dynamic;
+
+            StartCoroutine(FallAfterDelay());
 		}
 	}
+
+    IEnumerator FallAfterDelay() 
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb.isKinematic = false;
+    }
 }
