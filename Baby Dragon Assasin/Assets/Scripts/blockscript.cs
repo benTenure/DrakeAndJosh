8db4ae21d56b361407d6b1750330fb8454c37d8c
Unknown <shelbym1@umbc.edu>
2018-04-13 14:08:28 -0400

    update

---
+++ Baby Dragon Assasin/Assets/Scripts/blockscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockscript : MonoBehaviour {
+
+	public Sprite [] sheet;
+	private int minTexture = 0;
+	private int maxTexture = 25;
+
+	SpriteRenderer blocks;
+
+
+	// Use this for initialization
+	void Start () {
+		blocks = GetComponent<SpriteRenderer> ();
+		int random = Random.Range (minTexture, maxTexture);
+		blocks.sprite = sheet [random];
+	}
+
+	// Update is called once per frame
+
+
+
+}
