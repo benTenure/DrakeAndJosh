cd4b433ab0e4117fa2ff9c5e8744017fa790a254
Unknown <ben.mozilla@gmail.com>
2018-04-13 09:43:44 -0400

    Stealth Stuff Started

--- Baby Dragon Assasin/Assets/Scripts/LavaWallScript.cs
+++ Baby Dragon Assasin/Assets/Scripts/LavaWallScript.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class LavaWallScript : MonoBehaviour {
 
-    private Vector3 currentPos;
     private Vector3 newPos;
 
 
 	// Use this for initialization
-	void Start () {
-        currentPos = transform.position;
-        newPos = new Vector3(transform.position.x + 20, 0, 0);
+	void Start () 
+        {     
+                newPos = new Vector3(transform.position.x + 20, 0, 0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+        {
 
-        //Move one unit in the x-direction
-        transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime);
+                //Move one unit in the x-direction
+                transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime);
 
-        //After 20 units stop moving
+                //After 20 units stop moving
 
 	}
 }
---
+++ Baby Dragon Assasin/Assets/Scripts/PlayerStealthScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStealthScript : MonoBehaviour {
+
+	private bool isHiding;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) 
+	{
+		if (coll.tag == "Cover") {
+			Debug.Log("I can hide here!");
+		}
+	}
+}
