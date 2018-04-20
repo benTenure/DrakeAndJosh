188fce44dbe26241fa1ea2789bbd84ba808e62a4
Daniel Engbert <end1@umbc.edu>
2018-04-20 18:21:28 -0400

    Got projectile working (still need to make it do damage and disappear).

---
+++ Baby Dragon Assasin/Assets/Scripts/BallProjectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallProjectile : MonoBehaviour {
+    private float existenceTime = 0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("time: " + existenceTime);
+        existenceTime += Time.deltaTime;
+        // after 0.7 seconds, have this object dissapear
+        if (existenceTime > 0.7)
+        {
+            // TODO: get this working
+            //Object.Destroy(this.gameObject);
+        }
+    }
+}
--- Baby Dragon Assasin/Assets/Scripts/DustBunny.cs
+++ Baby Dragon Assasin/Assets/Scripts/DustBunny.cs
@@ -7,14 +7,21 @@ public class DustBunny : MonoBehaviour {
     private Animator anim;
     private bool facingRight = false;
     private Transform playerTrans; // player transform
+    public Rigidbody2D ballPrefab;
+    private float lastAttackTime = 0f;
+
+    // projectile stuff https://www.youtube.com/watch?v=KKgtC_Gy65c
+    public LayerMask whatToHit;
 
     void Start() {
+        // TODO: add timeout to the balls so that they disappear eventually if you miss
         playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
+        lastAttackTime += Time.deltaTime;
         // check distance from player
         if (playerTrans)
         {
@@ -26,7 +33,9 @@ public class DustBunny : MonoBehaviour {
 
             // attack if the player is too close
             if (Mathf.Abs(xDist) < 5f && Mathf.Abs(yDist) < 1f)
+            {
                 ToggleAttack(true);
+            }
             else
                 ToggleAttack(false);
 
@@ -42,10 +51,41 @@ public class DustBunny : MonoBehaviour {
         }
     }
 
+    // do a projectile attack towards the player
+    private void attack()
+    {
+
+        // source and destination points for the projectile
+        Vector2 dest = new Vector2(playerTrans.position.x, playerTrans.position.y);
+        Vector2 src = new Vector2(this.transform.position.x, this.transform.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(src, dest-src, 5, whatToHit);
+        Debug.DrawLine(src, dest);
+        Debug.Log("\nattacking from " + src + " to " + dest);
+        // note for later: under the dust bunny script in the inpspector, you can choose which layers it can be hit
+        // (right now it's just set to player, (I put the player on the player layer in its settings))
+        
+        // only shoot a projectile if we can "see" the player
+        if (hit.collider != null) // something hit
+        {
+            Debug.DrawLine(src, hit.point, Color.red);
+            // create a ball object
+            // TODO: consider making the ball not a rigidbody, just have a collider that will trigger it to disappear
+            ballPrefab = Instantiate(ballPrefab) as Rigidbody2D;
+            ballPrefab.transform.position = transform.position; // spawn at location of dust bunny
+            ballPrefab.velocity = 5*(dest-src);
+        }
+    }
+
     // control whether the dust bunny is attacking or not
     void ToggleAttack(bool state)
     {
         anim.SetBool("attacking", state);
+        // attack with a projectile up to once every 1.5 seconds
+        if (lastAttackTime > 1.5)
+        {
+            attack();
+            lastAttackTime = 0;
+        }
     }
 
     // flip the bunny to face the opposite way
@@ -54,4 +94,5 @@ public class DustBunny : MonoBehaviour {
         anim.transform.Rotate(0, 180, 0);
         facingRight = !facingRight;
     }
+
 }
