cbb26ccd70de49916e741bc8f822fd075315c80d
Unknown <ben.mozilla@gmail.com>
2018-04-20 20:11:39 -0400

    Holy Shit Update (April 20, 2018)
    Health UI, new playroom song, physics material for player added, animated UI, GameOver/Win scenes. All we need is the new music for the lose/win screens, enemyattack/damage/bounce/flashred!

--- Baby Dragon Assasin/Assets/Scripts/GameManagerScript.cs
+++ Baby Dragon Assasin/Assets/Scripts/GameManagerScript.cs
@@ -5,23 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour {
 
-	public HelathSystemScript playerHealth;
+    public HelathSystemScript playerHealth;
     public GameObject player;
+    public GameObject heartR;
+    public GameObject heartM;
+    public GameObject heartL;
 	public float restartDelay = 5f;
 
 	private float restartTimer;
+    private Animator animR;
+    private Animator animM;
+    private Animator animL;
+
+	private void Awake()
+	{
+        //Probably the worst possible way of doing this, but it'll work for now
+        animR = heartR.GetComponent<Animator>();
+        animM = heartM.GetComponent<Animator>();
+        animL = heartL.GetComponent<Animator>();
+	}
 
 	void Update () {
 
+
+
         //Restarts level when player dies
-		if (playerHealth.currentHealth <= 0) 
+		if (playerHealth.tries <= 0) 
 		{
             RestartLevel();
 		}
 
         if (player.transform.position.y <= -10) {
+            playerHealth.TakeDamage();
             RestartLevel();
         }
+
+        //Controls the health/hearts UI for the player
+        if (playerHealth.tries == 2) {
+            animR.SetBool("isHurt", true);
+        }
+        if (playerHealth.tries == 1) {
+            animM.SetBool("isHurt", true);
+        }
+        if (playerHealth.tries == 0)
+        {
+            animL.SetBool("isHurt", true);
+        }
+
+
 	}
 
     void RestartLevel() {
@@ -30,7 +61,7 @@ public class GameManagerScript : MonoBehaviour {
 
         if (restartTimer >= restartDelay)
         {
-            SceneManager.LoadScene("Level00");
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
--- Baby Dragon Assasin/Assets/Scripts/HelathSystemScript.cs
+++ Baby Dragon Assasin/Assets/Scripts/HelathSystemScript.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class HelathSystemScript : MonoBehaviour {
 
-	public int startingHealth = 2;
+    //You run out of tries before lives (2 tries gone = 1 life gone)
+    public int tries = 3;
+    public int lives = 1;
+
+	public int startingHealth = 1;
 	public int currentHealth;
 	public int playerLives = 1;
+    public float thrust = 10f;
 
+    private bool damaged;
 	private bool isDead;
+
+    private Rigidbody2D rb2d;
 
 	void Awake() 
 	{
-		//Start the player at full health
-		currentHealth = startingHealth;
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -21,10 +28,17 @@ public class HelathSystemScript : MonoBehaviour {
 	{
 		if (isDead)
 		{
-			Debug.Log("Oh no, I died");
-			gameObject.SetActive(false);
-			Debug.Log("Game Over");
+			//Debug.Log("Oh no, I died");
+			//gameObject.SetActive(false);
+			//Debug.Log("Game Over");
+
+            //
+
 		}
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            TakeDamage();
+        }
 	}
 
 	//Check for collision
@@ -37,19 +51,45 @@ public class HelathSystemScript : MonoBehaviour {
 		}
 	}
 
-	void TakeDamage() 
+	public void TakeDamage() 
 	{
-		currentHealth--;
+        //Boolean that is checked in the Update() for flashing red
+        damaged = true;
 
-		if (currentHealth >= 1 && !isDead) 
-		{
-			Debug.Log("Current Health: " + currentHealth);
+        if (tries >= 1 && !isDead) {
+
+            //Hurt the player
+		    tries--;
+
+            //Bounce the player (to the left for now)
+            rb2d.AddForce(-transform.right * thrust);
+
+			Debug.Log("Current Health: " + tries);
 		}
-		else
+        else if (tries == 0 && lives >= 0)
 		{
-			isDead = true;
+			
+            isDead = true;
 		}
 	}
 
+    //private void HurtPlayer() {
+    //  if (tries > 0 && lives > 0)
+    //  {
+    //      //Hurt the player.
+    //      tries--;
+    //      //Code that will bounce the player in the opposite direction of what hurt him.
+    //  }
+    //  else if (tries == 0 && lives > 0)
+    //  {
+    //      //Take away a life and reset the player's tries
+    //      lives--;
+    //      tries = 1;
+    //      //Code that will reset player to most recent checkpoint
+    //  }
+    //  else {
+    //      //Code that will trigger a game over screen
+    //  }
+    //}
 
 }
